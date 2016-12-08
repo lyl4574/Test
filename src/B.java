0011353342c58df7ae8535f5c918526f03e114f3
lyl4574 <liyunlong4574@icloud.com>
2016-12-08 16:52:36 +0800

    test

--- src/B.java
+++ src/B.java
@@ -1,6 +1,6 @@
 
 public class B extends A {
-	 public int method1(int a, int b) { return 0; }
+	 public int method1(int a, int b) { return 0;  }
 //	private int method1(int a, int b) { return 0; }
 	 private int method1(int a, long b) { return 0; }
 //	public short method1(int a, int b) { return 0; }
