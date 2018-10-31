e75774131e9d642abb776cbb0ee4742eda615fe7
lyl <liyunlong4574@163.com>
2018-10-31 16:11:32 +0800

    没用了

--- src/Test2.java
+++
@@ -1,8 +0,0 @@
-
-public class Test2 {
-public static void main(String[] args) {
-	String path="financing/sendBargain/32895/10.00/";
-	String a = path.replaceAll("\\d+", "{seqno}");
-	System.out.println(a);
-}
-}
