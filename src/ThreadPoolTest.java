9335f343614daa998637cd4d42d7021a6a9c1790
lyl <liyunlong4574@163.com>
2018-11-01 14:02:28 +0800

    没用了

--- src/ThreadPoolTest.java
+++ src/ThreadPoolTest.java
@@ -3,7 +3,7 @@ import zookeeper.Executor;
 import java.util.concurrent.*;
 
 public class ThreadPoolTest {
-    static int count=0;
+    private static int count=0;
     public static void main(String[] args) {
         ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(2,
                 8, 0L, TimeUnit.MILLISECONDS,
