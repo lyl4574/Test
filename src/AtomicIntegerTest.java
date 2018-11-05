dbdcc247629ce68bf7e4b2dc8c4d8dfb0b8942a0
lyl <liyunlong4574@163.com>
2018-11-05 14:51:14 +0800

    --

--- src/AtomicIntegerTest.java
+++ src/AtomicIntegerTest.java
@@ -1,6 +1,10 @@
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.LongAdder;
-// java 8 及以上推荐使用 LongAdder
+/* java 8 及以上推荐使用 LongAdder
+锁和synchronized需要通过操作系统来仲裁谁获得锁，开销比较高，
+        而AtomicInteger是通过CPU级的CAS操作来保证原子性，
+        开销比较小。所以使用AtomicInteger的目的还是为了提高性能。
+        */
 public class AtomicIntegerTest {
     private static int int_count =0;
     public static void main(String[] args) {
