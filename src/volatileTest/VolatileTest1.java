aaba0217e4eb8e1d176bd417eb7ba20e74c3b66f
lyl <liyunlong4574@163.com>
2018-11-05 11:30:49 +0800

    --

--- src/volatileTest/VolatileTest1.java
+++ src/volatileTest/VolatileTest1.java
@@ -1,34 +1,28 @@
 package volatileTest;
 
 public class VolatileTest1 {
-    private static boolean isRunning = false;
-    static class Stop extends Thread {
-        public void run(){
-            try {
-                Thread.sleep(1000);
-                isRunning = false;
-                System.out.println("thread is stop"+Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+    private  volatile boolean isRunning = true;
+    public void stop(){
+        isRunning = false;
+        System.out.println("modify running false");
     }
-    static class Start extends Thread {
+     class Start extends Thread {
         public void run(){
-            isRunning = true;
             while (isRunning){
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
                 System.out.println("thread is running"+Thread.currentThread().getName());
             }
         }
     }
 
     public static void main(String[] args) {
-        new Start().start();
-        new Stop().start();
+        VolatileTest1 vt = new VolatileTest1() ;
+       vt.new Start().start();
+       vt.new Start().start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        vt.stop();
     }
 }
