287b033519f19b225d43faf71cf598182fc67c4a
lyl <liyunlong4574@163.com>
2018-11-05 18:14:20 +0800

    --

---
+++ src/multipleThreadComunication/PhaserDemo.java
@@ -0,0 +1,31 @@
+package multipleThreadComunication;
+
+import java.io.IOException;
+import java.util.concurrent.Phaser;
+/*
+多个线程必须等到其它线程的同一阶段的任务全部完成才能进行到下一个阶段，并且每当完成某一阶段任务时，Phaser都会执行其onAdvance方法。
+ */
+public class PhaserDemo {
+  public static void main(String[] args) throws IOException {
+    int parties = 2;
+    int phases = 5;
+    final Phaser phaser = new Phaser(parties) {
+      @Override  
+      protected boolean onAdvance(int phase, int registeredParties) {  
+          System.out.println("====== Phase : " + phase + " ======");  
+          return registeredParties == 0;  
+      }  
+    };
+    
+    for(int i = 0; i < parties; i++) {
+      int threadId = i;
+      Thread thread = new Thread(() -> {
+        for(int phase = 0; phase < phases; phase++) {
+          System.out.println(String.format("Thread %s, phase %s", threadId, phase));
+          phaser.arriveAndAwaitAdvance();
+        }
+      });
+      thread.start();
+    }
+  }
+}
