3b262faf0fe688e8e917c6f7e1ae4f195fbc7973
lyl <liyunlong4574@163.com>
2018-11-06 17:41:17 +0800

    --

--- src/SystemPropertiesTest.java
+++ src/SystemPropertiesTest.java
@@ -10,6 +10,8 @@ public class SystemPropertiesTest {
 		for(Object o :set){
 			System.out.println(o.toString() + "|" + System.getProperty(o.toString()));
 		}
-		
+		System.out.println(Runtime.getRuntime().maxMemory()/1024.0/1024);
+		System.out.println(Runtime.getRuntime().freeMemory()/1024.0/1024);
+		System.out.println(Runtime.getRuntime().totalMemory()/1024.0/1024);
 	}
 }
