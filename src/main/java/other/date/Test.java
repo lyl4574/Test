ecabb556c68b724fa6df8a26400dd7ba221bcc5f
lyl <liyunlong4574@163.com>
2019-05-14 11:41:33 +0800

    --

---
+++ src/main/java/other/date/Test.java
@@ -0,0 +1,45 @@
+package other.date;
+
+import java.time.*;
+
+public class Test {
+    public static void main(String[] args) {
+        final Clock clock = Clock.systemUTC();
+        System.out.println(System.currentTimeMillis());
+        System.out.println(clock.instant());
+        System.out.println(clock.millis());
+
+        // Get the local date and local time
+        final LocalDate date = LocalDate.now();
+        final LocalDate dateFromClock = LocalDate.now( clock );
+
+        System.out.println( date );
+        System.out.println( dateFromClock );
+
+        // Get the local date and local time
+        final LocalTime time = LocalTime.now();
+        final LocalTime timeFromClock = LocalTime.now( clock );
+
+        System.out.println( time );
+        System.out.println( timeFromClock );
+
+
+        final LocalDateTime datetime = LocalDateTime.now();
+        final LocalDateTime datetimeFromClock = LocalDateTime.now( clock );
+
+        System.out.println( datetime );
+        System.out.println( datetimeFromClock );
+
+        // Get the zoned date/time
+        final ZonedDateTime zonedDatetime = ZonedDateTime.now();
+        final ZonedDateTime zonedDatetimeFromClock = ZonedDateTime.now( clock );
+        final ZonedDateTime zonedDatetimeFromZone = ZonedDateTime.now( ZoneId.of( "America/Los_Angeles" ) );
+
+        System.out.println( zonedDatetime );
+        System.out.println( zonedDatetimeFromClock );
+        System.out.println( zonedDatetimeFromZone );
+
+        //计算时间差 Duration.between( from, to );
+    }
+
+}
