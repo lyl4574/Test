dde0480314c6854b00b00bdf4be1e738c0834353
lyl <liyunlong4574@163.com>
2019-05-15 12:20:52 +0800

    --

--- src/main/java/design/single/S2.java
+++ src/main/java/design/single/S2.java
@@ -6,6 +6,7 @@ JDK 版本：JDK1.5 起
 实现难度：较复杂
 描述：这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
 getInstance() 的性能对应用程序很关键。
+如：sun\misc\Launcher.class.ExtClassLoader.
  */
 public class S2 {
     private volatile static S2 singleton;
