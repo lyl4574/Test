2ccfab52e22b39a332afa9102471b6790664e8e9
lyl4574 <liyunlong4574@icloud.com>
2019-05-23 11:30:43 +0800

    --

---
+++ src/main/java/io/Test.java
@@ -0,0 +1,44 @@
+package io;
+
+import jdk.internal.util.xml.impl.Input;
+
+import java.io.*;
+
+public class Test {
+    public static void main(String[] args) throws IOException {
+//        byteReadWrite();
+        stringReadWrite();
+    }
+
+    /**
+     * 字节流读取，每次读取1kb；
+     * @throws IOException
+     */
+    public static void byteReadWrite()throws IOException {
+        InputStream fis =  new FileInputStream("D:\\ss.txt");
+        OutputStream fos = new FileOutputStream("D:\\target.txt");
+        byte[] bytes = new byte[1024];
+        while (fis.read(bytes) != -1) {
+            fos.write(bytes);
+        }
+        fos.flush();
+        fis.close();
+        fos.close();
+    }
+
+    /**
+     * 字符流读取，每次读取1k字符；乱码
+     * @throws IOException
+     */
+    public static void stringReadWrite()throws IOException {
+        Reader reader =  new FileReader("D:\\ss.txt");
+        Writer writer = new FileWriter("D:\\target.txt");
+        char[] chars = new char[1024];
+        while (reader.read(chars) != -1) {
+            writer.write(chars);
+        }
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
